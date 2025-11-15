34d7d855e71d54fc0f02da76d7bbdf146d36ff87
Jairik <jairikmc@gmail.com>
2025-11-15 17:45:14 -0500

    Fixing some random config stuff

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { Provider } from './components/ui/provider'
 import { createRoot } from 'react-dom/client'
-import { Provider } from "./components/ui/provider"
 import App from './App.tsx'
 import './index.css'
 
@@ -10,5 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <Provider>
       <App />
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
