1d26515c55f5176f29bfabbe8434812344a8ec26
Jason-Sh1 <160532008+Jason-Sh1@users.noreply.github.com>
2025-11-15 19:19:40 -0500

    Purchase Analyzer
    Purchase Analyzer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { Provider } from './components/ui/provider'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './PurchaseAnalyzer.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
